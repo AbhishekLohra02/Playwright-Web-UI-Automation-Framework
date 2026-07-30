8d39b81aa34b727f6cd8f27c6e9d8b1f45912639
s0601298 <abhisheklohra02@gmail.com>
2026-07-30 23:46:05 +0200

    adding first Automated tests for Authentication

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,3 +4,15 @@ from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 from test_data.users import STANDARD_USERNAME, PASSWORD
 
+## TC-AUTH-001
+
+def test_successful_login_with_standard_user(page: Page, base_url: str) -> None:
+    login_page = LoginPage(page)
+    inventory_page = InventoryPage(page)
+
+    login_page.open(base_url)
+    login_page.login(STANDARD_USERNAME, PASSWORD)
+    expect(inventory_page.page_title).to_be_visible()
+    expect(inventory_page.page_title).to_have_text("Products")
+
+
